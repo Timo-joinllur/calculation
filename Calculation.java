8198ddbd99fcb08661b4c0137e013a698e77426f
Timo-joinllur <111872301+Timo-joinllur@users.noreply.github.com>
2022-12-15 14:53:09 +0200

    calculation class

---
+++ Calculation.java
@@ -0,0 +1,18 @@
+
+public class Calculation {
+	
+
+	    public static double calcArea(double width, double length){
+
+	        return width*length;
+
+	    }
+
+	    public static double calcArea(double radius){
+
+	        return Math.PI*radius*radius;
+
+	    }
+
+	
+}
---
+++ Hui.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Hui {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+
+
+        while (true) {
+
+            System.out.println("Calculate an area of a rectangle or circle (r/c)?\nx to exit");
+
+            String choice = sc.nextLine();
+
+            if (choice.equals("r")) {
+
+                System.out.println("Width?");
+
+                double width = Double.parseDouble(sc.nextLine());
+
+                System.out.println("Length?");
+
+                double length = Double.parseDouble(sc.nextLine());
+
+                double res = Calculation.calcArea(width, length);
+
+                System.out.println(res);
+
+                System.out.printf("Formatted: %.2f \n", res);
+
+            }
+
+            else if (choice.equals("c")) {
+
+                System.out.println("Radius?");
+
+                double r = Double.parseDouble(sc.nextLine());
+
+                double res = Calculation.calcArea(r);
+
+                System.out.println(res);
+
+                System.out.printf("Formatted output: %.2f \n", res);
+
+            }
+
+            else if (choice.equals("x"))
+
+                break;
+
+            else
+
+                System.out.println("Unknown choice. Please try again.");
+
+        }
+
+    }
+	}
+
+
+
